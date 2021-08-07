f8751e997e6fd6433794dabb5807cc652ad6f816
Al3gatsu <ramsabat.lml@gmail.com>
2021-08-06 22:13:26 -0500

    Una sola función

--- conversor_monedas.py
+++ conversor_monedas.py
@@ -1,29 +1,31 @@
+# Conversor de monedas
+
 print("Selecciona una de las siguientes opciones.")
 print(''' [1] Convertir peso Mx a dolar Us\n [2] Convertir dolar Us a peso Mx\n''')
 opcion = int(input("Opcion: "))
 
 
-def conversion_pesos_dolar(pesos):
-    valor_dolar = 19.95
-    dolares = pesos / valor_dolar
-    dolares = round(dolares,2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
-
-def conversion_dolares_peso(dolar):
-    valor_peso = 1/(19.95)
-    pesos = dolar/valor_peso
-    pesos = round(pesos,2)
-    pesos = str(pesos)
-    print("Tienes $" + pesos + " pesos")   
+def conversion(moneda,cantidad):
+    if moneda == "dolares":
+        factor = 19.95
+    elif moneda == "pesos":
+        factor = 1/(19.95)
+    else:
+        print("Intentelo de nuevo ;)")
+    convertido = cantidad/factor
+    convertido = round(convertido,2)
+    convertido = str(convertido)
+    print(f'Tienes {convertido} {moneda}')
 
 
 if opcion == 1:
     print("Convertir peso Mx a dolar Us\n")
     pesos = float(input("Cantidad de pesos: "))
-    conversion_pesos_dolar(pesos)
+    opcion = "dolares"
+    conversion(opcion,pesos)
 elif opcion == 2:
     print("Convertir dolar Us a peso Mx\n")
     dolar = float(input("Cantidad de dolares: "))
-    conversion_dolares_peso(dolar)
+    opcion = "pesos"
+    conversion(opcion,dolar)
 
