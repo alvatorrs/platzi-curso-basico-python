8e5874b970a2f7c84439bb2222579935d144b527
Al3gatsu <ramsabat.lml@gmail.com>
2021-09-05 19:55:53 -0500

    Última actualización

--- conversor_monedas.py
+++ conversor_monedas.py
@@ -1,7 +1,4 @@
 # Conversor de monedas
-print("Selecciona una de las siguientes opciones.")
-print(''' [1] Convertir peso Mx a dolar Us\n [2] Convertir dolar Us a peso Mx\n''')
-opcion = int(input("Opcion: "))
 
 
 def conversion(moneda,cantidad):
@@ -9,22 +6,31 @@ def conversion(moneda,cantidad):
         factor = 19.95
     elif moneda == "pesos":
         factor = 1/(19.95)
-    else:
-        print("Intentelo de nuevo ;)")
     convertido = cantidad/factor
     convertido = round(convertido,2)
     convertido = str(convertido)
     print(f'Tienes {convertido} {moneda}')
 
 
-if opcion == 1:
-    print("Convertir peso Mx a dolar Us\n")
-    pesos = float(input("Cantidad de pesos: "))
-    opcion = "dolares"
-    conversion(opcion,pesos)
-elif opcion == 2:
-    print("Convertir dolar Us a peso Mx\n")
-    dolar = float(input("Cantidad de dolares: "))
-    opcion = "pesos"
-    conversion(opcion,dolar)
+def main():
+    print("Selecciona una de las siguientes opciones.")
+    print(''' [1] Convertir peso Mx a dolar Us
+ [2] Convertir dolar Us a peso Mx\n''')
+    opcion = int(input("Opcion: "))
 
+    if opcion == 1:
+        print("Convertir peso Mx a dolar Us\n")
+        pesos = float(input("Cantidad de pesos: "))
+        opcion = "dolares"
+        conversion(opcion,pesos)
+    elif opcion == 2:
+        print("Convertir dolar Us a peso Mx\n")
+        dolar = float(input("Cantidad de dolares: "))
+        opcion = "pesos"
+        conversion(opcion,dolar)
+    else:
+        print("Intentelo de nuevo :)")
+        main()
+
+if __name__ == '__main__':
+    main()
